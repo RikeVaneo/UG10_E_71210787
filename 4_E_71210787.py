9964cf39c9735b575d57ade536ef54527ba80f06
Rike <rike.vaneo@ti.ukdw.ac.id>
2021-11-18 21:28:27 +0700

    menambahkan soal 4

---
+++ 4_E_71210787.py
@@ -0,0 +1,18 @@
+a = int(input("Masukkan bilangan 1 : "))
+b = int(input("Masukkan bilangan 2 : "))
+c = int(input("Masukkan bilangan 3 : "))
+if (a<=b) and (a<=c):
+    if b<=c:
+        print ("Urutan bilangan dari yang terkecil adalah",a,b,c)
+    else:
+        print("Urutan bilangan dari yang terkecil adalah",a,c,b)
+elif (b<=a) and (b<=c):
+    if a<=c:
+        print("Urutan bilangan dari yang terkecil adalah",b,a,c)
+    else:
+        print("Urutan bilangan dari yang terkecil adalah",b,c,a)
+elif (c<=a) and (c<=b):
+    if a<=b:
+        print("Urutan bilangan dari yang terkecil adalah",c,a,b)
+    else:
+        print("Urutan bilangan dari yang terkecil adalah",c,b,a)
